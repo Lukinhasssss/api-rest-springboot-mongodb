df3cf2d6f3c6850bcd7b9cbd0b296ec72e6678ac
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-02-15 11:44:00 -0300

    User insert

--- src/main/java/com/lucasmonteiro/apimongodb/services/UserService.java
+++ src/main/java/com/lucasmonteiro/apimongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lucasmonteiro.apimongodb.domain.User;
+import com.lucasmonteiro.apimongodb.dto.UserDTO;
 import com.lucasmonteiro.apimongodb.repositories.UserRepository;
 import com.lucasmonteiro.apimongodb.services.exceptions.ObjectNotFoundException;
 
@@ -23,6 +24,15 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = repository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User user) {
+		return repository.insert(user);
+	}
+	
+	// Método que pega um DTO e instancia um usuário
+	public User fromDTO(UserDTO userDTO) {
+		return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
 	}
 
 }
