20057ec5c546480ccf7a1051ac0e94ac888d6d18
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-02-15 13:49:46 -0300

    Post findById

---
+++ src/main/java/com/lucasmonteiro/apimongodb/services/PostService.java
@@ -0,0 +1,23 @@
+package com.lucasmonteiro.apimongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lucasmonteiro.apimongodb.domain.Post;
+import com.lucasmonteiro.apimongodb.repositories.PostRepository;
+import com.lucasmonteiro.apimongodb.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repository;
+	
+	public Post findById(String id) {
+		Optional<Post> post = repository.findById(id);
+		return post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
