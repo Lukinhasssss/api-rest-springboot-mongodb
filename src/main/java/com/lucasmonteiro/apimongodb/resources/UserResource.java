b2cad7f029219f8bab50bcee64faa95beae20227
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-02-15 13:41:08 -0300

    Endpoint /users/{id}/posts to return posts of an user

--- src/main/java/com/lucasmonteiro/apimongodb/resources/UserResource.java
+++ src/main/java/com/lucasmonteiro/apimongodb/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.lucasmonteiro.apimongodb.domain.Post;
 import com.lucasmonteiro.apimongodb.domain.User;
 import com.lucasmonteiro.apimongodb.dto.UserDTO;
 import com.lucasmonteiro.apimongodb.services.UserService;
@@ -56,12 +57,18 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 	}
 	
-	@PutMapping
+	@PutMapping(value = "/{id}")
  	public ResponseEntity<Void> update(@RequestBody UserDTO userDTO, @PathVariable String id) {
 		User user = service.fromDTO(userDTO);
 		user.setId(id);
 		user = service.update(user);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(user.getPosts());
 	}
 
 }
