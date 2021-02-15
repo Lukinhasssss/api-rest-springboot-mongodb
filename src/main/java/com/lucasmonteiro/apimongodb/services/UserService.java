94422348e546ab5688c47fb6c019d573b4ef0155
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-02-15 11:28:48 -0300

    User findById and exceptions

--- src/main/java/com/lucasmonteiro/apimongodb/services/UserService.java
+++ src/main/java/com/lucasmonteiro/apimongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.lucasmonteiro.apimongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lucasmonteiro.apimongodb.domain.User;
 import com.lucasmonteiro.apimongodb.repositories.UserRepository;
+import com.lucasmonteiro.apimongodb.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
 }
